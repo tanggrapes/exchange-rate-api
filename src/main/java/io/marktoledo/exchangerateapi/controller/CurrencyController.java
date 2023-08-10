279e60fed34ebbfca2204c696fa1cf5254d5a36a
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-08-11 03:27:41 +0800

    controller

--- src/main/java/io/marktoledo/exchangerateapi/controller/CurrencyController.java
+++ src/main/java/io/marktoledo/exchangerateapi/controller/CurrencyController.java
@@ -1,2 +1,31 @@
-package io.marktoledo.exchangerateapi.controller;public class CurrencyController {
+package io.marktoledo.exchangerateapi.controller;
+
+
+import io.marktoledo.exchangerateapi.dto.Currency;
+import io.marktoledo.exchangerateapi.thirdparty.ExchangeRateProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/currencies")
+public class CurrencyController {
+
+    private ExchangeRateProxy exchangeRateProxy;
+
+    @Value("${third-party.exchange-rates-api.api-key}")
+    private String apiKey;
+
+    @Autowired
+    public CurrencyController(ExchangeRateProxy exchangeRateProxy){
+        this.exchangeRateProxy = exchangeRateProxy;
+    }
+
+    @GetMapping
+    public Currency getCurrencies(){
+        return this.exchangeRateProxy.getCurrencies(this.apiKey);
+    }
+
 }
--- src/main/java/io/marktoledo/exchangerateapi/controller/RateController.java
+++ src/main/java/io/marktoledo/exchangerateapi/controller/RateController.java
@@ -1,2 +1,32 @@
-package io.marktoledo.exchangerateapi.controller;public class RateController {
+package io.marktoledo.exchangerateapi.controller;
+
+
+import io.marktoledo.exchangerateapi.dto.Rate;
+import io.marktoledo.exchangerateapi.thirdparty.ExchangeRateProxy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/rates")
+public class RateController {
+
+    private ExchangeRateProxy exchangeRateProxy;
+
+    @Value("${third-party.exchange-rates-api.api-key}")
+    private String apiKey;
+
+    @Autowired
+    public RateController(ExchangeRateProxy exchangeRateProxy){
+        this.exchangeRateProxy = exchangeRateProxy;
+    }
+
+    @GetMapping("")
+    public Rate getRate(@RequestParam("base") String base, @RequestParam("symbols") String[] symbols){
+        return this.exchangeRateProxy.getRate(this.apiKey,base, String.join(",", symbols));
+    }
 }
+

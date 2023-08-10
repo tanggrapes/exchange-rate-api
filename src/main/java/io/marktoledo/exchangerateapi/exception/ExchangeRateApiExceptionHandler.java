fb25152b1ad034f42463039fa9b7b76e3c214872
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-08-11 03:28:03 +0800

    handle feign exception

--- src/main/java/io/marktoledo/exchangerateapi/exception/ExchangeRateApiExceptionHandler.java
+++ src/main/java/io/marktoledo/exchangerateapi/exception/ExchangeRateApiExceptionHandler.java
@@ -1,2 +1,23 @@
-package io.marktoledo.exchangerateapi.exception;public class ExchangeRateApiExceptionHandler {
+package io.marktoledo.exchangerateapi.exception;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import feign.FeignException;
+import io.marktoledo.exchangerateapi.dto.ErrorResponse;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class ExchangeRateApiExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(FeignException.class)
+    public ResponseEntity<ErrorResponse> handleError(FeignException exception, WebRequest request) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ErrorResponse errorResponse = objectMapper.readValue(exception.contentUTF8(), ErrorResponse.class);
+        return new ResponseEntity<>(errorResponse, HttpStatusCode.valueOf(exception.status()));
+    }
 }

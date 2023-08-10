f55d9e8427b6e0d36afe395e7b40f68c2ff09789
Mark Toledo <markjaysonprietotoledo@gmail.com>
2023-08-11 03:27:33 +0800

    3rd party integration using feign

--- src/main/java/io/marktoledo/exchangerateapi/thirdparty/ExchangeRateProxy.java
+++ src/main/java/io/marktoledo/exchangerateapi/thirdparty/ExchangeRateProxy.java
@@ -1,2 +1,17 @@
-package io.marktoledo.exchangerateapi.thirdparty;public interface ExchangeRateProxy {
+package io.marktoledo.exchangerateapi.thirdparty;
+
+import io.marktoledo.exchangerateapi.dto.Currency;
+import io.marktoledo.exchangerateapi.dto.Rate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name="exchange-rate", url ="http://api.exchangeratesapi.io/v1")
+public interface ExchangeRateProxy {
+
+    @GetMapping("/symbols?access_key={apiKey}")
+    Currency getCurrencies(@PathVariable String apiKey);
+
+    @GetMapping("/latest?access_key={apiKey}&base={base}&symbols={symbols}")
+    Rate getRate(@PathVariable String apiKey, @PathVariable String base, @PathVariable String symbols);
 }
